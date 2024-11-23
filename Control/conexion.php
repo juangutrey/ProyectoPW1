972011f3b4eea35eecb22bda8cd94dc3a415d2e7
juangutrey <juangurey@gmail.com>
2024-11-23 01:45:45 -0600

    Cambios del proyecto para MV

--- Control/conexion.php
+++ Control/conexion.php
@@ -15,16 +15,30 @@
 
     // Configuración para InfinityFree
     //$host_db = "sql206.infinityfree.com";
-    $user_name = "if0_37732853";
-    $user_pass = "kjlqQRMxciUvmj";
-    $db_name = "if0_37732853_gimnasio_db";
+    //$user_name = "if0_37732853";
+    //$user_pass = "kjlqQRMxciUvmj";
+    //$db_name = "if0_37732853_gimnasio_db";
 
     // Crear la conexión
-    $conexion = new mysqli($host_db, $user_name, $user_pass, $db_name);
+    //$conexion = new mysqli($host_db, $user_name, $user_pass, $db_name);
 
     // Verificar si la conexión fue exitosa
-    if ($conexion->connect_error) {
-    die("<h1>Error de conexión: " . $conexion->connect_error . "</h1>");
+    //if ($conexion->connect_error) {
+    //die("<h1>Error de conexión: " . $conexion->connect_error . "</h1>");
+    //} else {
+    //echo "<h1></h1>";
+    //}
+
+    // Configuración para MV
+    $servername = "localhost";
+    $username = "juan";
+    $password = "Corazon.2";
+    $dbname = "prueba_bd";
+// Crear conexión
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    Verificar si la conexión fue exitosa
+    if ($conn->connect_error) {
+    die("<h1>Error de conexión: " . $conn->connect_error . "</h1>");
     } else {
     echo "<h1></h1>";
     }
