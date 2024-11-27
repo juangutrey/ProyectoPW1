89174990ebe38820b45b24ad70624f04c3b392dd
Juangurey <juangutrey@gmail.com>
2024-11-26 21:54:11 -0600

    Mas cambios

--- Control/conexion.php
+++ Control/conexion.php
@@ -1,19 +1,19 @@
 <?php
 // Configuración de conexión
-$host_db = "localhost";
-$user_name = "huronmarron";
-$user_pass = "123456789";
-$db_name = "prueba_bd";
+$servername = "localhost";
+$username = "huronmarron";
+$password = "123456789";
+$dbname = "prueba_bd";
 
-// Crear la conexión
-$conexion = new mysqli($host_db, $user_name, $user_pass, $db_name);
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar si la conexión fue exitosa
-if ($conexion->connect_error) {
-    die("<h1>Error de conexión: " . $conexion->connect_error . "</h1>");
-} else {
-    echo "<h1>Conexion exitosa</h1>";
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
 }
+echo "Conexión exitosa a la base de datos";
+
 // Cerrar conexión
 $conn->close();
 ?>
