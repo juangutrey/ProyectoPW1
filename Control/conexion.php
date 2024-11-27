cfcfbb22426688ab56352d176537234c50dc71a3
Juangurey <juangutrey@gmail.com>
2024-11-26 20:35:24 -0600

    Mas cambios

--- Control/conexion.php
+++ Control/conexion.php
@@ -1,47 +1,20 @@
 <?php
-//para servidor local:
-    //$host_db="127.0.0.1";
-    //$user_name="root";
-    //$user_pass="";
-    //$db_name="gimnasio_db";
+// Configuración de conexión
+$servername = "localhost";
+$username = "huronmarron";
+$password = "123456789";
+$dbname = "prueba_bd";
 
-    //$conexion = new mysqli($host_db,$user_name,$user_pass, $db_name);
-    //if( $conexion -> connect_error){
-    //    echo "<h1>Error de conexion </h1> ";
-    //}
-    //else{
-    //   echo "<h1></h1>";
-    //}
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Configuración para InfinityFree
-    //$host_db = "sql206.infinityfree.com";
-    //$user_name = "if0_37732853";
-    //$user_pass = "kjlqQRMxciUvmj";
-    //$db_name = "if0_37732853_gimnasio_db";
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+echo "Conexión exitosa a la base de datos";
 
-    // Crear la conexión
-    //$conexion = new mysqli($host_db, $user_name, $user_pass, $db_name);
-
-    // Verificar si la conexión fue exitosa
-    //if ($conexion->connect_error) {
-    //die("<h1>Error de conexión: " . $conexion->connect_error . "</h1>");
-    //} else {
-    //echo "<h1></h1>";
-    //}
-
-    // Configuración para MV
-    $servername = "localhost";
-    $username = "huronmarron";
-    $password = "123456";
-    $dbname = "huronmarron";
-    
-    // Crear conexión
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    
-    // Verificar si la conexión fue exitosa
-    if ($conn->connect_error) {
-        die("<h1>Error de conexión: " . $conn->connect_error . "</h1>");
-    } else {
-        echo "<h1>Conexión exitosa</h1>";
-    }
+// Cerrar conexión
+$conn->close();
 ?>
+
